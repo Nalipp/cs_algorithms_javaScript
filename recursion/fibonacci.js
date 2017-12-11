bdfe3f245581abfa094f46e9a79325c39dcd962d
Nalipp <nateplipp@gmail.com>
2017-12-11 14:03:16 -0600

    recursive memonization

--- recursion/fibonacci.js
+++ recursion/fibonacci.js
@@ -3,12 +3,15 @@
 
 // [1, 1, 2, 3, 5, 8, 13, 21, 34]
 
-let getFib = (position) => {
-  if (position < 0) throw new Error('position must be not be negative');
-  if (position <= 1) return position;
-  return getFib(position - 1) + getFib(position - 2);
+let getFib = (n, obj={}) => {
+  if (n < 0) throw new Error('n must be not be negative');
+  if (n <= 1) return n;
+  let first = obj[n-1] ? obj[n-1] : obj[n-1] = getFib(n-1, obj);
+  let second = obj[n-2] ? obj[n-2] : obj[n-2] = getFib(n-2, obj);
+  console.log(first, second)
+  return first + second;
 }
 
-console.log(getFib(9));
+// console.log(getFib(9));
 console.log(getFib(11));
-console.log(getFib(0));
+// console.log(getFib(0));
